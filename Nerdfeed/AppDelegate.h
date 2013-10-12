55fd4e231121540dd95fc565b1152d144bafce44
Ryan Case <rmcase@gmail.com>
2013-10-11 18:31:22 -0600

    Initial Commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Ryan Case on 10/11/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NerdfeedTests/NerdfeedTests.m
@@ -0,0 +1,34 @@
+//
+//  NerdfeedTests.m
+//  NerdfeedTests
+//
+//  Created by Ryan Case on 10/11/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NerdfeedTests : XCTestCase
+
+@end
+
+@implementation NerdfeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
