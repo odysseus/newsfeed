8dd60e74640b8a27c44b02f1cc1a77239601f4ff
Ryan Case <rmcase@gmail.com>
2013-10-17 18:34:21 -0600

    Finished implementing the connection class.

--- Nerdfeed/BNRConnection.m
+++ Nerdfeed/BNRConnection.m
@@ -10,4 +10,71 @@
 
 @implementation BNRConnection
 
+@synthesize request, completionBlock, xmlRootObject;
+
+static NSMutableArray *sharedConnectionList = nil;
+
+- (id)initWithRequest:(NSURLRequest *)req
+{
+    self = [super init];
+    if (self) {
+        [self setRequest:req];
+    }
+    return self;
+}
+
+- (void)start
+{
+    // Initialize container for data collected from NSURLConnection
+    container = [[NSMutableData alloc] init];
+    
+    // Spawn connection
+    internalConnection = [[NSURLConnection alloc] initWithRequest:[self request]
+                                                         delegate:self
+                                                 startImmediately:YES];
+    
+    // If this is the first connection started, create the array
+    if (!sharedConnectionList)
+        sharedConnectionList = [[NSMutableArray alloc] init];
+    
+    // Add the connection to the array so it doesn't get destroyed
+    [sharedConnectionList addObject:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [container appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // If there is a "root object"
+    if ([self xmlRootObject]) {
+        
+        // Create a parser with the incoming data and let the root object parse
+        // its contents
+        NSXMLParser *parser = [[NSXMLParser alloc] initWithData:container];
+        [parser setDelegate:[self xmlRootObject]];
+        [parser parse];
+    }
+    
+    // Then, pass the root object to the completion block
+    if ([self completionBlock])
+        [self completionBlock]([self xmlRootObject], nil);
+    
+    // Now, destroy this connection
+    [sharedConnectionList removeObject:self];
+}
+
+- (void)connection:(NSURLConnection *)connection
+  didFailWithError:(NSError *)error
+{
+    // Pass the error from the connection to the completionBlock
+    if ([self completionBlock])
+        [self completionBlock](nil, error);
+    
+    // Destroy this connection
+    [sharedConnectionList removeObject:self];
+}
+
 @end
--- Nerdfeed/BNRFeedStore.m
+++ Nerdfeed/BNRFeedStore.m
@@ -28,6 +28,21 @@
                   @"&feed_type=RSS2.0&feed_style=COMPACT"];
     
     NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    // Create an empty channel
+    RSSChannel *channel = [[RSSChannel alloc] init];
+    
+    // Create a connection "actor" object that will transfer data from the server
+    BNRConnection *connection = [[BNRConnection alloc] initWithRequest:req];
+    
+    // When the connection completes, this block from the controller will be called
+    [connection setCompletionBlock:block];
+    
+    // Let the empty channel parse the returning data from the web service
+    [connection setXmlRootObject:channel];
+    
+    // Begin the connection
+    [connection start];
 }
 
 @end
